2398ec859c2dbc675ef6f4bf2238865967c76063
Bridgette <bridgye@gmail.com>
2015-04-03 08:40:11 -0700

    longest_substring
    Initial commit

---
+++ longest_substring.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr 03 08:36:01 2015
+
+@author: bridgette
+"""
+import unittest
+
+class Longest_Substring_Solution:
+    '''
+    Given a string, find the length 
+    of the longest substring without repeating characters.    
+    '''
+
+class Longest_Substring_Test(unittest.TestCase):
+    def test_example(self):
+        answer = ""
+        expected = ""
+        self.assertEqual(answer, expected)
+
+if __name__ == '__main__':
+    unittest.main()
