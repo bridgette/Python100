0e454a36d9f137ade921b08cc7e1c4061af927c4
Bridgette <bridgye@gmail.com>
2015-09-26 13:52:02 -0700

    string repeat

---
+++ string_repeat.py
@@ -0,0 +1,49 @@
+'''
+Determines if a string has all unique letters.
+'''
+
+import unittest
+
+class Solution:
+    
+    def test_unique(self, s):
+        ''' Uses a dictionary to test uniqueness
+        of letters in string.
+        '''
+        d = {}
+        for char in s:
+            if not str.isalpha(char):
+                continue
+            if char in d.keys():
+                return True
+            else:
+                d[char] = 1
+            
+        return False
+
+
+class Unique_Characters_Test(unittest.TestCase):
+
+    def test_negative(self):
+        s = Solution()
+        input_s = "abc defghi jklmnop qrs tuvwxyz!"
+        expected = False
+        answer = s.test_unique(input_s)
+        self.assertEqual(answer, expected)
+        
+    def test_negative_punc(self):
+        s = Solution()
+        input_s = "abc defghi jklmnop qrs tuvwxyz!!!"
+        expected = False
+        answer = s.test_unique(input_s)
+        self.assertEqual(answer, expected)
+        
+    def test_positive(self):
+        s = Solution()
+        input_s = "fizzbuzz"
+        expected = True
+        answer = s.test_unique(input_s)
+        self.assertEqual(answer, expected)   
+        
+if __name__ == '__main__':
+    unittest.main()
