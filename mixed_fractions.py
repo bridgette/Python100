9001b88832540a43f23858dbd2dba213b0f753be
Bridgette <bridgye@gmail.com>
2015-10-06 15:03:34 -0700

    mixed fractions

--- mixed_fractions.py
+++ mixed_fractions.py
@@ -16,22 +16,47 @@ change 3/12 to 1/4).
 import unittest
 
 class MixedFractionsSolution(object):
-    def mixedFractions(self, numerator, denominator):
+    def mixedFractions(self, fractionAsString):
         '''
-        input:
+        input: a string with two integers, seperated by a space
+        output: string displaying mixed fraction as a whole number, space, 
+            followed by a proper fraction
         '''
-                
-        return
+        numerator, denominator = fractionAsString.split(" ")
+        print numerator, denominator
+        
+        numerator = int(numerator)
+        denominator = int(denominator)
+        
+        wholenum = 0
+        remainder = 0
+        
+        if numerator >= denominator:
+            wholenum = numerator / denominator
+        remainder = numerator % denominator
+            
+        return "{0} {1}/{2}".format(str(wholenum), str(remainder), str(denominator))
         
         
-class EightQueensTest(unittest.TestCase):
+class MixedFractionsTest(unittest.TestCase):
     
     def test1(self):
         s = MixedFractionsSolution()
-        answer = s.mixedFractions(27, 12)
+        answer = s.mixedFractions("27 12")
         expected = "2 3/12"
         self.assertEqual(answer, expected)
         
+    def test2(self):
+        s = MixedFractionsSolution()
+        answer = s.mixedFractions("2460000 98400")
+        expected = "25 0/98400"
+        self.assertEqual(answer, expected)
+        
+    def test3(self):
+        s = MixedFractionsSolution()
+        answer = s.mixedFractions("3 4000")
+        expected = "0 3/4000"
+        self.assertEqual(answer, expected)
         
         
 if __name__ == '__main__':
