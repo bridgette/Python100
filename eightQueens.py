3d21701d3f1aaa93fe5b69c877c3130206aef92a
Bridgette <bridgye@gmail.com>
2015-10-06 11:10:01 -0700

    eight queens solution
    O(n^2)

--- eightQueens.py
+++ eightQueens.py
@@ -7,50 +7,79 @@ Created on Mon Oct 5th 2015
 import unittest
 
 class EightQueensSolution(object):
+    '''
+    input: board (string with 8 rows x 8 chars)
+    Given a chessboard where *s are Queens
+    and .s are spaces, return valid if no Queens
+    can attack each other and invalid if Queens can 
+    attack each other.
+    '''
+        
     def eightQueens(self, board):
         '''
-        input: board (string with 8 rows x 8 chars)
-        Given a chessboard where *s are Queens
-        and .s are spaces, return valid if no Queens
-        can attack each other and invalid if Queens can 
-        attack each other.
+        Solves Eight Queens problem by remembering which rows, columns,
+        and diagonals upon which we have seen Queens. Runs in O(n^2), where n
+        is height/width of chessboard.
         '''
-        chess_board = []
-        each_row = board.split('\n')
-                
-        return "valid"
+        columns_seen = {}
+        rows_seen = {}
+        left_diag_seen = {}
+        right_diag_seen = {}
         
+        all_rows = board.split('\n')
+        for i, row in enumerate(all_rows):
+            print "I see a row ", row, "at i ", i, ""
+            for j, square in enumerate(list(row)):
+                
+                if square == '*':
+                    if j in columns_seen:
+                        return "invalid"
+                    elif i in rows_seen:
+                        return "invalid"
+                    elif i-j in left_diag_seen:
+                        return "invalid"
+                    elif i+j in right_diag_seen:
+                        return "invalid"
+                    else:
+                        columns_seen[j] = True
+                        rows_seen[i] = True
+                        left_diag_seen[i-j] = True
+                        right_diag_seen[i+j] = True
+                            
+        return "valid"
+    
         
 class EightQueensTest(unittest.TestCase):
     
     def test1(self):
         s = EightQueensSolution()
-        answer = s.eightQueens("*.......\
-                                ..*.....\
-                                ....*...\
-                                ......*.\
-                                .*......\
-                                .......*\
-                                .....*..\
-                                ...*....")
-        expected = "valid"
+        answer = s.eightQueens("*.......\n\
+..*.....\n\
+....*...\n\
+......*.\n\
+.*......\n\
+.......*\n\
+.....*..\n\
+...*....")
+        expected = "invalid"
         self.assertEqual(answer, expected)
         
     def test2(self):
         s = EightQueensSolution()
-        answer = s.eightQueens("*.......\
-                                ......*.\
-                                ....*...\
-                                .......*\
-                                .*......\
-                                ...*....\
-                                .....*..\
-                                ..*.....")
-        expected = "invalid"
+        answer = s.eightQueens("*.......\n\
+......*.\n\
+....*...\n\
+.......*\n\
+.*......\n\
+...*....\n\
+.....*..\n\
+..*.....")
+        expected = "valid"
         self.assertEqual(answer, expected)
         
         
 if __name__ == '__main__':
     unittest.main()
+    # lines = [line.rstrip('\n') for line in open('filename')]
 
         
