94d0867973efc395c8a8c39f42e90826ff01c046
Bridgette <bridgye@gmail.com>
2015-10-13 21:09:27 -0700

    Bot Saves Princess I
    Bot saves princess working

--- bot_saves_princess.py
+++ bot_saves_princess.py
@@ -31,28 +31,50 @@ class PrincessSolution():
         '''
         Uses breadth-first-search to find a way to princess peach.
         n: number of rows and columns in the square grid
-        grid: a string representing the contents of every square in the grid       
+        grid: string representing the contents of every square in the grid       
         '''
-        # convert the grid (string) into a more usable matrix
+        matrix, hero, princess = self.ConvertGridToMatrix(3, grid)
+        
+        q = Queue.Queue()
+        valid_path = self.BFS(matrix, hero, princess, q)
+        
+        directions = []
+        
+        for step in range(1, len(valid_path)):
+            prev_step = valid_path[step-1]
+            curr_step = valid_path[step]
+            d = self.ShoutDirections(prev_step, curr_step)
+            directions.append(d)
+            
+        return directions.join(" ")
+    
+    def ConvertGridToMatrix(self, n, grid):
+        '''
+        grid: grid, represented by a string
+        n: numberof rows and columns in a square grid
+
+        returns a tuple with:        
+            - the grid as a more usable matrix
+            - the (x,y) position of the hero
+            - the (x,y) position of the princess
+        '''
         matrix = [[0 for r in range(n)] for c in range(n)]
+        hero_pos = ()
+        princess_pos = ()
         
         string_position = 0
         for r in range(n):
             for c in range(n):
-                print grid[string_position], string_position
+
+                if grid[string_position] == 'm':
+                    hero_pos = (r,c)
+                elif grid[string_position] == 'p':
+                    princess_pos = (r,c)
+                    
                 matrix[r][c] = grid[string_position]
                 string_position += 1
                 
-        print matrix
-        
-        # the hero always starts in the center of the grid
-        hero = (n/2, n/2)
-        print matrix[hero[0]][hero[1]]
-        
-        q = Queue.Queue()
-        
-        
-        return ""
+        return (matrix, hero_pos, princess_pos)
         
     def BFS(self, graph, start, end, q):
         '''
@@ -63,40 +85,99 @@ class PrincessSolution():
         end: tuple of (x,y) princess start position
         q: the Queue used for BFS        
         '''
-    	temp_path = [start]
+        temp_path = [start]
+        q.put(temp_path)
     	
-    	q.enqueue(temp_path)
-    	
-    	while q.IsEmpty() == False:
-    		tmp_path = q.dequeue()
-    		last_node = tmp_path[len(tmp_path)-1]
-    		print tmp_path
+        while q.empty() == False:
+            tmp_path = q.get()
+            last_node = tmp_path[len(tmp_path)-1]
       
-    		if last_node == end:
-    			print "VALID_PATH : ",tmp_path
+    	    if last_node == end:
+             return tmp_path
 
-    		for link_node in neighbors(graph, last_node):
-    			if link_node not in tmp_path:
-    				new_path = []
-    				new_path = tmp_path + [link_node]
-    				q.enqueue(new_path)
+            for link_node in self.GetHeroMoves(graph, last_node):
+                if link_node not in tmp_path:
+                    new_path = []
+                    new_path = tmp_path + [link_node]
+                    q.put(new_path)
         
-    def neighbors(grid, position):
+    def GetHeroMoves(self, grid, position):
         ''' 
-        returns all valid neighbors of a position on a grid.
-        takes into account the edges of the grid
+        Returns a list of all valid positions the hero can move to on the grid.
+        Hero cannot fall off the grid!
+        Hero cannot move diagonally!
         '''
-        return
+        x, y = position        
+        neighbors = []
+        grid_size = len(grid) # grid is square!
+        
+        # up
+        if (x > 0):
+            neighbors.append((x-1, y))
+            
+        # left
+        if (y > 0):
+            neighbors.append((x, y - 1))
+            
+        # right
+        if (y + 1 > 0):
+            neighbors.append((x, y + 1))
+            
+        # down
+        if (x + 1 < grid_size):
+            neighbors.append((x + 1, y))
+        
+        return neighbors
+        
+    def ShoutDirections(self, current, nex):
+        '''
+        shout directions at the hero so he can find his way.
+        required output form.
+        '''
+        curr_x, curr_y= current
+        next_x, next_y = nex
+        if (curr_x > next_x):
+            return "DOWN"
+        elif (curr_x < next_x):
+            return "UP"
+        elif (curr_y < next_y):
+            return "LEFT"
+        elif (curr_y > next_y):
+            return "RIGHT"
         
 
 class MagicalStringTest(unittest.TestCase):
     
-    def test1(self):
-        s = PrincessSolution()
-        answer = s.displayPathtoPrincess(3, "p---m-----")
-        expected = ""
+    def setUp(self):
+        self.s = PrincessSolution()
+        self.threeXthree = "p---m-----"
+        self.fourXfour = "p---------m-----"
+        
+    def test_hero_moves(self):
+        matrix, _, _ = self.s.ConvertGridToMatrix(3, self.threeXthree)
+        
+        answer = self.s.GetHeroMoves(matrix, (0,0))
+        expected = [(0,1), (1,0)]
         self.assertEqual(answer, expected)
         
+        answer = self.s.GetHeroMoves(matrix, (1,1))
+        expected = [(0,1), \
+                    (1,0), (1,2), \
+                    (2,1)]
+        self.assertEqual(answer, expected)
+        
+        answer = self.s.GetHeroMoves(matrix, (2,2))
+        expected = [(1,2), (2,1), (2,3)]
+        self.assertEqual(answer, expected) 
+        
+    def integrated_test(self):
+        answer = self.s.displayPathtoPrincess(3, self.threeXthree)
+        expected0 = "UP LEFT"
+        expected1 = "LEFT UP"
+        self.assertEqual(answer, expected0) or self.assertEqual(answer, expected1)
+        
+
+
 if __name__ == '__main__':
     unittest.main()
 
