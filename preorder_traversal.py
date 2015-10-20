8ac70dc392057f4f61441077dd07a2af937d5299
Bridgette <bridgette@outlook.com>
2015-10-19 21:52:46 -0700

    PreOrder Tree Check
    2014 ICPC North Central Regional - North America

--- preorder_traversal.py
+++ preorder_traversal.py
@@ -29,18 +29,90 @@ preorder traversal of a binary search tree.
 
 import unittest
 
+class Node:
+    def __init__(self, val):
+        self.l = None
+        self.r = None
+        self.v = val
+
+class Tree:
+    def __init__(self):
+        self.root = None
+
+    def getRoot(self):
+        return self.root
+
+    def add(self, val):
+        if(self.root == None):
+            self.root = Node(val)
+        else:
+            self._add(val, self.root)
+
+    def _add(self, val, node):
+        if(val < node.v):
+            if(node.l != None):
+                self._add(val, node.l)
+            else:
+                node.l = Node(val)
+        else:
+            if(node.r != None):
+                self._add(val, node.r)
+            else:
+                node.r = Node(val)
+
+    def find(self, val):
+        if(self.root != None):
+            return self._find(val, self.root)
+        else:
+            return None
+
+    def _find(self, val, node):
+        if(val == node.v):
+            return node
+        elif(val < node.v and node.l != None):
+            self._find(val, node.l)
+        elif(val > node.v and node.r != None):
+            self._find(val, node.r)
+
+    def preOrderTree(self):
+        '''
+        Uses tail recursion to get the preordered list of the binary tree.
+        '''
+        if(self.root != None):
+            return self._preOrderTree(self.root, [])
+
+    def _preOrderTree(self, node, preordered_list):
+        if(node == None):
+            return preordered_list
+        preordered_list.append(node.v)
+        preordered_list = self._preOrderTree(node.l, preordered_list)
+        preordered_list = self._preOrderTree(node.r, preordered_list)
+        return preordered_list
+
+      
 class PreorderTraversalSolution():
-    
+          
     def CheckPreorder(self, l):
         '''
         Returns whether list l represents a valid preorder of a binary 
-        search tree.
+        search tree, by inserting all elements of l into a binary tree
+        and comparing the preorder of the tree to the order of l.
+        
+        Building the binary search tree takes O(n^2), and traversing it takes
+        O(n). Total runtime is then O(n^2).
         
         output: "yes" or "no"
         '''
-        return "yes"
-
-
+        if len(l) > len(set(l)): return "no"
+        
+        tree = Tree()
+        for each_l in l:
+            tree.add(each_l)
+        preordered_list = tree.preOrderTree()
+        if preordered_list == l:
+            return "yes"
+        else:
+            return "no"
    
 
 class PreorderTraversalTester(unittest.TestCase):
@@ -48,16 +120,41 @@ class PreorderTraversalTester(unittest.TestCase):
     def setUp(self):
         self.s = PreorderTraversalSolution()
 
-
-    def test(self):     
+    def test_123(self):     
         answer = self.s.CheckPreorder([2,3,1])
         expected = "no"
         self.assertEqual(answer, expected) 
         
-        answer = self.s.CheckPreorder([50, 30, 20, 10, 25, 40, 45, 70, 90, 80])
+        answer = self.s.CheckPreorder([1, 2, 3])
         expected = "yes"
         self.assertEqual(answer, expected) 
         
+        answer = self.s.CheckPreorder([1, 3, 2])
+        expected = "yes"
+        self.assertEqual(answer, expected) 
+        
+        answer = self.s.CheckPreorder([2, 1, 3])
+        expected = "yes"
+        self.assertEqual(answer, expected) 
+        
+        answer = self.s.CheckPreorder([3, 2, 1])
+        expected = "yes"
+        self.assertEqual(answer, expected) 
+        
+        answer = self.s.CheckPreorder([3, 1, 2])
+        expected = "yes"
+        self.assertEqual(answer, expected) 
+        
+    def test_dupes(self):
+        answer = self.s.CheckPreorder([3, 3, 1, 2])
+        expected = "no"
+        self.assertEqual(answer, expected) 
+        
+    def test_medium(self):
+        answer = self.s.CheckPreorder([50, 30, 20, 10, 25, 40, 45, 70, 90, 80])
+        expected = "yes"
+        self.assertEqual(answer, expected) 
+
 
 
 if __name__ == '__main__':
@@ -68,10 +165,15 @@ s = PreorderTraversalSolution()
 case = 0
 while True:
     try:
+        tree = []
         numbers_on_line = raw_input().split()
         for n in numbers_on_line:
-            case = case + 1
-            print "Case", str(case) + ":", s.CalcTotalLoops_Naive(int(n))
+            if int(n) >= 0:
+                tree.append(int(n))
+            if int(n) < 0:
+                case = case + 1
+                print "Case", str(case) + ":", s.CheckPreorder(tree)
+                tree = []
     except EOFError as e:
         break
 '''
